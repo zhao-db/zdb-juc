e5ef605fef2a8c367e7249ff268be920617075c9
dongbao.zhao <dongbao.zhao@geekplus.com>
2022-04-21 17:16:56 +0800

    juc test

---
+++ src/test/java/com/example/zdbjuc/ZdbJucApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zdbjuc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZdbJucApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
