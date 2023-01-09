78ec3ec9cb6d71ce0774e551a9394f33394ce283
zhaodb <dongbao.zhao@geekplus.com>
2023-01-09 14:09:31 +0800

    init

---
+++ src/main/java/com/example/zdbjuc/ZdbJucApplication.java
@@ -0,0 +1,13 @@
+package com.example.zdbjuc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdbJucApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZdbJucApplication.class, args);
+    }
+
+}
